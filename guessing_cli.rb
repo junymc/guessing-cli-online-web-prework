763e3c30def4960b1ad30a1e0265c08f3f5fa2e3
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 19:52:54 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,12 +1,12 @@
-def run_guessing_game(com_number)
+def run_guessing_game
   puts "Guess a number between 1 and 6."
     number = gets.chomp
-    if(com_number == number)
+    if( == number)
       puts "You guessed the correct number!"
     elsif(number == "exit")
       puts "Goodbye!"
     else
-      puts "The computer guessed #{com_number}."
+      puts "The computer guessed #{}."
     end
 
 end
